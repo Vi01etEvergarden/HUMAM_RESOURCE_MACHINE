40ebe48bbb230672877ba28d4d66d94a03396e66
yedou37 <yiedou1leo@163.com>
2023-10-31 23:25:10 +0800

    add project files

---
+++ game.hpp
@@ -0,0 +1,45 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+
+/*
+need global variables:
+	int	boxInHand
+	char inConv[]
+	char outConv[]	
+	char area[]      //using "char" to save 1~9 and 'X' 
+					//using atio() to transfer a char 'num' to a real num which can be involved into computation 
+
+need functions :
+	
+	init interface
+	level selection
+	
+	input commands (from FILE* or stdin)
+	clear screen
+	std print screen
+	
+	inBox
+	outBox
+	addBox
+	subBox
+	copyTo
+	copyFrom
+	jumpTo
+	jumpIfZero
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+
+#include "game.hpp"
+
+
+
+
+
+
+using namespace std;
+int main()
+{
+	
+	
+	
+	
+	return 0;
+}
